8d37409bb0a3e94c1c61e0c5fb4e3873ff94a0a0
MadikhanSadykov <madikhansadykov@gmail.com>
2023-10-02 16:12:27 +0600

    Creating the ID system for modals

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  private visible = false
+  private modals: IModal[] = []
   constructor() {
 
   }
 
-  isModalVisible() {
-    return this.visible
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
   }
 
-  toggleModal() {
-    this.visible = !this.visible
+  isModalVisible(id: string) : boolean{
+    return Boolean(this.modals.find(element => element.id === id)?.visible)
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+
+    if (modal) {
+      modal.visible = !modal.visible
+    }
+    // this.visible = !this.visible
   }
 
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {ModalService} from "../../services/modal.service";
 
 @Component({
@@ -9,12 +9,14 @@ import {ModalService} from "../../services/modal.service";
 })
 export class ModalComponent {
 
+  @Input() modalID = ''
+
   constructor(public modal: ModalService) {
 
   }
 
   closeModal($event: Event) {
     $event.preventDefault()
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 }
