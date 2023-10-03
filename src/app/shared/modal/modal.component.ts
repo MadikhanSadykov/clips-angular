7ea4c313c3f4ac05f1e5bf9e29a59b8f1fc462df
MadikhanSadykov <madikhansadykov@gmail.com>
2023-10-03 09:04:30 +0600

    css issues

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ElementRef, OnInit } from '@angular/core';
 import {ModalService} from "../../services/modal.service";
 
 @Component({
@@ -7,16 +7,21 @@ import {ModalService} from "../../services/modal.service";
   styleUrls: ['./modal.component.css'],
   // providers: [ModalService]
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
 
   @Input() modalID = ''
 
-  constructor(public modal: ModalService) {
-
+  constructor(public modal: ModalService, public elementRef: ElementRef) {
   }
 
   closeModal($event: Event) {
     $event.preventDefault()
     this.modal.toggleModal(this.modalID)
   }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.elementRef.nativeElement)
+  }
+
+
 }
